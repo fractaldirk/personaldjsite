7de69bddca9a5cef7f8197fd7e0efae2b2a3ed94
fractaldirk <dirkgrandjean@live.nl>
2013-07-05 17:42:50 +0200

    sign in with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Personaldj::Application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
+  get "home/index"
+  get "home/instructions"
+
   resources :lists
 
   resources :categories
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130704203655) do
+ActiveRecord::Schema.define(:version => 20130705150136) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -35,5 +35,15 @@ ActiveRecord::Schema.define(:version => 20130704203655) do
   end
 
   add_index "lists", ["club_id"], :name => "index_lists_on_club_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
 
 end
